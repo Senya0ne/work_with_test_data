0f1626c7866750c9d1ee186530d52d7dcbdb647e
Sergei Vasilchenko <liketravelin@gmail.com>
2020-05-11 20:21:37 +0300

    add changes for reader

--- src/reader.py
+++ src/reader.py
@@ -1,25 +1,34 @@
 import csv, json
-books = list()
-#  читаем csv
+
+from csv import DictReader
+
+books_input = []
 with open('../data/books.csv', newline='') as f:
-    reader = csv.reader(f)
-
-    # Извлекаем заголовок
-    header = next(reader)
-
+    reader = DictReader(f)
 
     # Итерируемся по данным делая из них словари
     for row in reader:
-        books = (dict(zip(header, row)))
-        print(books)
+        # print(row)
+        books_input = row
 
 
-print('*' * 100)
-#  читаем файл json
-with open("../data/users.json", "r") as f:
-    users = json.loads(f.read())
-print(users)
-    # for user in users:
-    #     dict = user['name'] + user['address'], user['gender']
-    #     print(dict)
+print(books_input)
+# with open("../data/users.json", 'r') as json_file:
+#     users_input = json.loads(json_file.read())
+#     print(users_input)
 
+
+
+
+
+# # запись в файл
+# with open("../data/users_output.json", 'w') as j_f:
+#     s = json.dumps(a, indent=4)
+#     j_f.write(s)
+
+
+# users_output = []
+# keys_sorting = ['name', 'gender', 'address']
+#
+# for keys_sorting in users_input:
+#     users = {}
